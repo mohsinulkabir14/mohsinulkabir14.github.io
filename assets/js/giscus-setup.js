395b2827ab38eb0efe1f8b85de7cbd91adfc9ff8
mohsinulkabir14 <45818756+mohsinulkabir14@users.noreply.github.com>
2025-11-09 22:17:44 +0000

    Deploying to gh-pages from @ mohsinulkabir14/mohsinulkabir14.github.io@03f56255f0446f17aac5f093282efbb440fb65aa 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "mohsinulkabir14/mohsinulkabir.github.io",
+    "data-repo": "mohsinulkabir14/mohsinulkabir14.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
